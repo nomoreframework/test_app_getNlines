31393e7727d4e9adad21e289501c617d9520c044
nomoreframework <gleblukianenko@gmail.com>
2023-10-09 08:36:02 +0300

    added simple CORS pol

--- get_posts_api/Program.cs
+++ get_posts_api/Program.cs
@@ -1,28 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-// builder.Services.AddCors(options =>
-//         {
-//             options.AddDefaultPolicy(builder =>
-//                 builder.SetIsOriginAllowed(_ => true)
-//                     .WithOrigins($"http://localhost")
-//                     .AllowAnyMethod()
-//                     .AllowAnyHeader()
-//                     .AllowCredentials());
-//         });
-if(builder.Environment.IsDevelopment()) 
-    builder.Services.AddSingleton<IPostRepository,PostRepository>();
+builder.Services.AddCors(options =>
+        {
+            options.AddDefaultPolicy(builder =>
+                builder.SetIsOriginAllowed(_ => true)
+                    .WithOrigins($"http://localhost")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+        });
+if(builder.Environment.IsDevelopment()) {
+    builder.Services.AddScoped<IDataContextStorage,DataContextStorage>();
+    builder.Services.AddScoped<IPostRepository,PostRepository>();
+}
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-
     app.UseSwagger();
     app.UseSwaggerUI();
 }
