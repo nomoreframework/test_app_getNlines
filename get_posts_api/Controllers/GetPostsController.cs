120bdbe9bd19bff08b3e9736fe7a89c7b9ffd0f6
nomoreframework <gleblukianenko@gmail.com>
2023-10-09 08:34:04 +0300

    fixed controller

--- get_posts_api/Controllers/GetPostsController.cs
+++ get_posts_api/Controllers/GetPostsController.cs
@@ -7,21 +7,27 @@ namespace test_app_getNlines.Controllers;
 [Route("api/[controller]/[action]")]
 public class GetPostsController : ControllerBase
 {
-    private readonly ILogger<GetPostsController> _logger;
+    private readonly ILogger<GetPostsController>? _logger;
     private readonly IPostRepository _rep;
-    public GetPostsController(IPostRepository rep,ILogger<GetPostsController> logger)
+    public GetPostsController(IPostRepository rep,ILogger<GetPostsController>? logger = null)
     {
         _logger = logger;
         _rep = rep;
     }
 
     [HttpGet]
-    public  async Task<ActionResult<IEnumerable<Post>>> GetNPostsByUserUIIId(Guid guid,int n){
-        var res = await Task.Run( () => _rep.GetLastNPostsByUserUIId(guid,n));
+    public  async Task<ActionResult<IEnumerable<Post>>> GetNPostsByUserUIId(Guid guid,int n, int pageNumber = 1){
+        var res = await Task.Run(() => _rep.GetLastNPostsByUserUIId(guid,n,pageNumber));
         return res != null ? Ok(res) : NotFound(guid);
     }
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<User>>> GetNLastUsersNoDeterminate(int count = 0){
+    public async Task<ActionResult<IEnumerable<Post>>> GetNLastPostsForUsers(int countOfLastUsers, int n, int pageNumber = 1){
+        var res = await Task.Run(()=> _rep.GetNLastPostsForUsers(countOfLastUsers,n,pageNumber));
+        return res != null ? Ok(res) : NotFound(res);
+    }
+    //дернуть для теста 
+    [HttpGet("hook")]
+    public async Task<ActionResult<IEnumerable<User>>> GetNLastUsersNoDeterminate(int count = 1){
         var res = await Task.Run(() =>_rep.GetNLastUsersNoDeterminate(count));
         return res != null ? Ok(res) : NotFound();
     }
